de513c3c6b947b5e5b56af6f347a79ec76d828c9
Necmettin Kizildag <necmettin_kzldg@hotmail.com>
2023-10-14 11:07:28 +0200

    fcPage, ReusableMEthods, day16 C03 are added

---
+++ src/test/java/pages/FacebookPage.java
@@ -0,0 +1,25 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class FacebookPage {
+
+    public FacebookPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "email")
+    public WebElement emaiLoginBox;
+
+    @FindBy(id = "pass")
+    public WebElement passwordLoginBox;
+
+    @FindBy(xpath = "//button[@name='login']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//button[@class='_42ft _4jy0 _al65 _4jy3 _4jy1 selected _51sy']")
+    public WebElement cookiesAllow;
+}
---
+++ src/test/java/tests/day16_testNG_framework/C03_FacebookTest.java
@@ -0,0 +1,38 @@
+package tests.day16_testNG_framework;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C03_FacebookTest {
+
+    @Test
+    public void facebookTest(){
+
+        //1 - https://www.facebook.com/ adresine gidin
+        Driver.getDriver().get("https://www.facebook.com/");
+
+        ReusableMethods.wait(1);
+
+        //2- POM’a uygun olarak email, sifre kutularini ve giris yap butonunu locate edin
+
+        //3- Faker class’ini kullanarak email ve sifre degerlerini yazdirip, giris butonuna basin
+        Faker faker = new Faker();
+        FacebookPage facebookPage = new FacebookPage();
+
+        facebookPage.cookiesAllow.click();
+
+        facebookPage.emaiLoginBox.sendKeys(faker.internet().emailAddress());
+
+        facebookPage.passwordLoginBox.sendKeys(faker.internet().password());
+
+        facebookPage.loginButton.click();
+
+
+
+        //4- Basarili giris yapilamadigini test edin
+
+    }
+}
