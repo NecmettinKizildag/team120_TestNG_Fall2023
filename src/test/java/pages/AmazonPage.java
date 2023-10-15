999ac8d4cab95f3563127d6b99b3b0089af59b91
Necmettin Kizildag <necmettin_kzldg@hotmail.com>
2023-10-15 08:48:13 +0200

    day17 C04 is aded

--- src/test/java/pages/AmazonPage.java
+++ src/test/java/pages/AmazonPage.java
@@ -28,4 +28,11 @@ public class AmazonPage {
     @FindBy(xpath = "//div[@class='a-section a-spacing-small a-spacing-top-small']")
     public WebElement resultTextElement;
 
+    @FindBy(xpath = "(//div[@class='sg-col-4-of-24 sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 sg-col s-widget-spacing-small sg-col-4-of-20'])[1]")
+    public WebElement firstItem;
+
+    @FindBy (xpath = "//h1[@id='title']")
+    public WebElement productTitle;
+
+
 }
---
+++ src/test/java/tests/day17_TestNGFramework_assertions/C04_Assertions.java
@@ -0,0 +1,48 @@
+package tests.day17_TestNGFramework_assertions;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AmazonPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C04_Assertions {
+
+    @Test
+    public void test01(){
+
+        // amazon anasayfaya gidelim
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+
+        // url'in amazon icerdigini test edelim
+        String expectedUrlContent = "amazon";
+        String actualUrl = Driver.getDriver().getCurrentUrl();
+
+        Assert.assertTrue(actualUrl.contains(expectedUrlContent));
+
+        // aranacak kelimeyi aratalim
+        AmazonPage amazonPage = new AmazonPage();
+
+        amazonPage.searchBox.click();
+
+        amazonPage.searchBox.sendKeys(ConfigReader.getProperty("amazonSearchWord")+Keys.ENTER);
+
+        // sonuclarin aranacak kelimeyi icerdigini test edelim
+        String actualResultText = amazonPage.resultTextElement.getText();
+        String expectedResultText = ConfigReader.getProperty("amazonSearchWord");
+
+        Assert.assertTrue(actualResultText.contains(expectedResultText));
+
+        // ilk urune tiklayalim
+        amazonPage.firstItem.click();
+
+        // ilk urun isminde aranacak kelime bulundugunu test edelim
+        String expectedProductTitle = ConfigReader.getProperty("amazonSearchWord");
+        String actualProductTitle = amazonPage.productTitle.getText();
+        Assert.assertTrue(actualProductTitle.contains(expectedProductTitle));
+
+        // sayfayi kapatalim
+        Driver.closeDriver();
+    }
+}
